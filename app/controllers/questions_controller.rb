25fe89b1eda42d6a7b955cade6174caeb6e73267
Erik Bonn & Lisa D'Asaro & Matthew Jones & Quyen Tran <pair+erikbonn.lmdasaro.matth3wjones.quyenttran@devbootcamp.com>
2017-05-18 12:27:49 -0700

    Add new and edit forms and controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,22 @@
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @question.view_count += 1
   erb :'/questions/show'
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb:"/questions/edit"
+end
+
+post '/questions' do
+  question = Question.new(params[:question])
+  "#{question}"
+  # if current_user.questions << question
+  # else
+  # end
 end
