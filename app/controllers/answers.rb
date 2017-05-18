7f5bd00f2bd7e5015dc607f069f780520bd6ea33
Matthew Jones <email": null>
2017-05-18 12:33:51 -0700

    add a comment controller, in progress

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,9 +1,24 @@
+before do
+  if logged_in?
+    @user = User.find(session[:id])
+  end
+end
+
 get '/answers' do
   @answers = Answer.all
   erb :_answers
 end
 
-get '/answers/new' do
+get '/questions/:id/answers/new' do
   @answer = Answer.new
+  @question = Question.find(params[:id])
   erb :_new_answer
+end
+
+post '/answers' do
+  puts "\n\n"
+  puts params
+  @answer = Answer.create!(params[answer])
+
+  redirect "/questions/#{@answer.question_id}"
 end
