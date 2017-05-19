aca5239dbc6363feeb73e1968afb3f78fe805a86
Erik Bonn <ebonn89@gmail.com>
2017-05-19 13:58:42 -0700

    tried to add logged in vote feature, failed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,17 @@
+before do
+  if logged_in?
+    @user = User.find(session[:id])
+    # session[:upvote_question_ids] = []
+  end
+end
+
 post '/questions/:id/upvote' do
+  # if @user.id == session[:id]
   @question = Question.find(params[:id])
+  # if session[:upvote_question_ids].include?(@question.id)
+  #   #ERRRor
+  # else
+  #  session[:upvote_question_id] << @question.id
   @question.votes << Vote.create({value: 1})
   if request.xhr?
     status 200
@@ -8,11 +20,14 @@ post '/questions/:id/upvote' do
   else
   redirect "/questions/#{@question.id}"
   end
+  # end
 end
 
 delete '/questions/:id/downvote' do
+  # if @user.id == session[:id]
   @question = Question.find(params[:id])
   last_vote = @question.votes.last
+  if !last_vote.nil?
   @question.votes.delete(last_vote)
    if request.xhr?
     status 200
@@ -21,5 +36,7 @@ delete '/questions/:id/downvote' do
   else
   redirect "/questions/#{@question.id}"
   end
+end
+# end
 end
 
