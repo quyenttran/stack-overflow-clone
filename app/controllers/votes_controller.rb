fe36ba01e855c93441793f3bb1cdacad0e21e328
Erik Bonn <ebonn89@gmail.com>
2017-05-19 12:09:37 -0700

    AJAX upvote and downvote buttons

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,25 @@
 post '/questions/:id/upvote' do
   @question = Question.find(params[:id])
   @question.votes << Vote.create({value: 1})
+  if request.xhr?
+    status 200
+    content_type :json
+    { question_id: @question.id, votecount: @question.votes.count }.to_json
+  else
   redirect "/questions/#{@question.id}"
+  end
 end
 
 delete '/questions/:id/downvote' do
   @question = Question.find(params[:id])
   last_vote = @question.votes.last
   @question.votes.delete(last_vote)
+   if request.xhr?
+    status 200
+    content_type :json
+    { question_id: @question.id, votecount: @question.votes.count }.to_json
+  else
   redirect "/questions/#{@question.id}"
+  end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+  $(".upvote-form").on("submit", function(e){
+      e.preventDefault();
+      var formUrl = $(this).attr('action');
+      var $rootDiv = $(this).closest('div');
 
+      $.ajax({
+        method: "post",
+        url: formUrl,
+        dataType: "json"
+      })
+      .done(function(postData){
+        $rootDiv.find(".vote-count").text(postData.votecount);
+      })
+  })
+
+  $(".downvote-form").on("submit", function(e){
+      e.preventDefault();
+      var formUrl = $(this).attr('action');
+      var $rootDiv = $(this).closest('div');
+
+      $.ajax({
+        method: "delete",
+        url: formUrl,
+        dataType: "json"
+      })
+      .done(function(postData){
+        $rootDiv.find(".vote-count").text(postData.votecount);
+      })
+  })
 
 });
